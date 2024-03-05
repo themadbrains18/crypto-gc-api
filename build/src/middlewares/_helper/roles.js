95f31477eca38f6e1a942c17d218f5a766fdc34a
themadbrains18 <70230188+themadbrains18@users.noreply.github.com>
2024-03-05 16:12:38 +0530

    Delete build directory

--- build/src/middlewares/_helper/roles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let roles = {
-    superadmin: "superadmin",
-    admin: "admin",
-    user: "user"
-};
-exports.default = roles;
--- build/src/models/dto/assets.dto.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// { walletTtype: '', balance: 0, account_type: '', token_id: '' };
