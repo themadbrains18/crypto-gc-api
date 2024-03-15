9d630908e8aca20a1ecf620ab3b1c7ce83ce4816
surinder <surinderkumar.mdb@gmail.com>
2024-03-15 16:14:30 +0530

    changes for convert coin

--- src/models/dal/convert.dal.ts
+++ src/models/dal/convert.dal.ts
@@ -8,6 +8,7 @@ import { assetsAccountType, assetsWalletType } from "../../utils/interface";
 import tokensModel from "../model/tokens.model";
 import globalTokensModel from "../model/global_token.model";
 import assetsDto from "../dto/assets.dto";
+import userModel from "../model/users.model";
 
 class convertDal {
 
@@ -23,89 +24,94 @@ class convertDal {
         // ===========================================
         // get  admin_ assets
         // ===========================================
-        let admin_assets = await assetModel.findAll({ where: { user_id: '3808e05f-7da6-441d-bf98-7b5ec864c694', walletTtype: assetsWalletType.main_wallet }, raw: true });
+        let adminUser = await userModel.findOne({ where: { role: 'admin' }, raw: true });
+        
+        if (adminUser) {
+            let admin_assets = await assetModel.findAll({ where: { user_id: adminUser.id, walletTtype: assetsWalletType.main_wallet }, raw: true });
 
-        let admin_consumption_asset = admin_assets.filter((item) => {
-            return item.token_id === payload.gain_token_id
-        })
-
-        let admin_gain_assets: any = admin_assets.filter((item) => {
-            return item.token_id === payload.consumption_token_id
-        })
-
-        let token = await tokensModel.findOne({ where: { id: payload.gain_token_id }, raw: true });
-        if (!token) {
-            token = await globalTokensModel.findOne({ where: { id: payload.gain_token_id }, raw: true });
-        }
-
-        if (admin_consumption_asset.length === 0) {
-            return { status: false, message: `${token?.symbol} Assets not available ` };
-        }
-        else if(admin_consumption_asset[0].balance < 0 || admin_consumption_asset[0].balance < payload.gain_amount){
-            return { status: false, message: `${token?.symbol} Insufficiant balance` };
-        }
-
-        // ===========================================
-        // convert data store db
-        // ===========================================
-        convertResponse = await convertModel.create(payload);
-
-        if (convertResponse) {
-            // ===========================================
-            // user assets update
-            // ===========================================
-            let assets = await assetModel.findAll({ where: { user_id: payload.user_id, walletTtype: assetsWalletType.main_wallet }, raw: true });
-            let consumption_asset = assets.filter((item) => {
-                return item.token_id === payload.consumption_token_id
-            })
-            let gain_assets: any = assets.filter((item) => {
+            let admin_consumption_asset = admin_assets.filter((item) => {
                 return item.token_id === payload.gain_token_id
             })
-            // consume user assets update
-            let newConBal: any = consumption_asset[0].balance - payload.consumption_amount;
-            let conResponse = await assetModel.update({ balance: newConBal.toFixed(8) }, { where: { id: consumption_asset[0].id } });
 
-            // gain user assets update
-            if (gain_assets.length > 0) {
-                let newBal: any = gain_assets[0]?.balance + payload.gain_amount;
-                let gainResponse = await assetModel.update({ balance: newBal.toFixed(8) }, { where: { id: gain_assets[0].id } });
+            let admin_gain_assets: any = admin_assets.filter((item) => {
+                return item.token_id === payload.consumption_token_id
+            })
+
+            let token = await tokensModel.findOne({ where: { id: payload.gain_token_id }, raw: true });
+            if (!token) {
+                token = await globalTokensModel.findOne({ where: { id: payload.gain_token_id }, raw: true });
             }
-            else {
-                let assets: assetsDto = {
-                    walletTtype: assetsWalletType.main_wallet,
-                    balance: payload.gain_amount,
-                    account_type: assetsAccountType.main_account,
-                    token_id: payload.gain_token_id,
-                    user_id: payload.user_id
-                };
-                let addNewAssets = await assetModel.create(assets);
+
+            if (admin_consumption_asset.length === 0) {
+                return { status: false, message: `${token?.symbol} Assets not available ` };
+            }
+            else if (admin_consumption_asset[0].balance < 0 || admin_consumption_asset[0].balance < payload.gain_amount) {
+                return { status: false, message: `${token?.symbol} Insufficiant balance` };
             }
 
             // ===========================================
-            //admin_ assets update
+            // convert data store db
             // ===========================================
+            convertResponse = await convertModel.create(payload);
 
-            // consume user assets update
-            let admin_newConBal: any = admin_consumption_asset[0].balance - payload.gain_amount;
-            let admin_conResponse = await assetModel.update({ balance: admin_newConBal.toFixed(8) }, { where: { id: admin_consumption_asset[0].id } });
+            if (convertResponse) {
+                // ===========================================
+                // user assets update
+                // ===========================================
+                let assets = await assetModel.findAll({ where: { user_id: payload.user_id, walletTtype: assetsWalletType.main_wallet }, raw: true });
+                let consumption_asset = assets.filter((item) => {
+                    return item.token_id === payload.consumption_token_id
+                })
+                let gain_assets: any = assets.filter((item) => {
+                    return item.token_id === payload.gain_token_id
+                })
+                // consume user assets update
+                let newConBal: any = consumption_asset[0].balance - payload.consumption_amount;
+                let conResponse = await assetModel.update({ balance: newConBal.toFixed(8) }, { where: { id: consumption_asset[0].id } });
 
-            // // gain user assets update
-            if (admin_gain_assets.length > 0) {
-                let admin_newBal: any = admin_gain_assets[0]?.balance + payload.consumption_amount;
-                let admin_gainResponse = await assetModel.update({ balance: admin_newBal.toFixed(8) }, { where: { id: admin_gain_assets[0].id } });
+                // gain user assets update
+                if (gain_assets.length > 0) {
+                    let newBal: any = gain_assets[0]?.balance + payload.gain_amount;
+                    let gainResponse = await assetModel.update({ balance: newBal.toFixed(8) }, { where: { id: gain_assets[0].id } });
+                }
+                else {
+                    let assets: assetsDto = {
+                        walletTtype: assetsWalletType.main_wallet,
+                        balance: payload.gain_amount,
+                        account_type: assetsAccountType.main_account,
+                        token_id: payload.gain_token_id,
+                        user_id: payload.user_id
+                    };
+                    let addNewAssets = await assetModel.create(assets);
+                }
+
+                // ===========================================
+                //admin_ assets update
+                // ===========================================
+
+                // consume user assets update
+                let admin_newConBal: any = admin_consumption_asset[0].balance - payload.gain_amount;
+                let admin_conResponse = await assetModel.update({ balance: admin_newConBal.toFixed(8) }, { where: { id: admin_consumption_asset[0].id } });
+
+                // // gain user assets update
+                if (admin_gain_assets.length > 0) {
+                    let admin_newBal: any = admin_gain_assets[0]?.balance + payload.consumption_amount;
+                    let admin_gainResponse = await assetModel.update({ balance: admin_newBal.toFixed(8) }, { where: { id: admin_gain_assets[0].id } });
+                }
+                else {
+                    let assets: assetsDto = {
+                        walletTtype: assetsWalletType.main_wallet,
+                        balance: payload.consumption_amount,
+                        account_type: assetsAccountType.main_account,
+                        token_id: payload.consumption_token_id,
+                        user_id: '3808e05f-7da6-441d-bf98-7b5ec864c694'
+                    };
+                    let addNewAssets = await assetModel.create(assets);
+                }
+
             }
-            else {
-                let assets: assetsDto = {
-                    walletTtype: assetsWalletType.main_wallet,
-                    balance: payload.consumption_amount,
-                    account_type: assetsAccountType.main_account,
-                    token_id: payload.consumption_token_id,
-                    user_id: '3808e05f-7da6-441d-bf98-7b5ec864c694'
-                };
-                let addNewAssets = await assetModel.create(assets);
-            }
-
         }
+
 
         return convertResponse;
     }
