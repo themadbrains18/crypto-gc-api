d702f1d1023ac27cee757661a6dd604cd79f7333
SakshiSethi0702 <sakshisethi.mdb@gmail.com>
2024-07-11 18:32:06 +0530

    update change

--- src/models/dal/users.dal.ts
+++ src/models/dal/users.dal.ts
@@ -1,6 +1,6 @@
 import { UserInput, UserOuput } from "../model/users.model";
 import sequelize, { assetModel, depositModel, globalTokensModel, lastLoginModel, marketOrderModel, orderModel, referProgramInviteModel, referUserModel, tokensModel, userModel, userRewardModel, userRewardTotalModel, walletsModel, withdrawModel } from "../index";
-import { Op } from "sequelize";
+import { col, fn, Op } from "sequelize";
 import { lastLoginOuput } from "../model/lastLogin.model";
 import MarketProfitModel, { MarketProfitOuput } from "../model/marketProfit.model";
 import service from "../../services/service";
@@ -60,10 +60,10 @@ class userDal {
           [Op.or]: [{ number: id }, { email: id }],
         },
       });
-   
+
       return user;
     } catch (error: any) {
-      
+
       throw new Error(error.message);
     }
   };
@@ -72,39 +72,166 @@ class userDal {
     try {
       const user = await userModel.findOne({
         where: {
-          own_code: refer
-        },
-      });
-      return user;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  };
+          [Op.and]: sequelize.where(fn('BINARY', col('own_code')), refer)
+      }
+        // where: fn('BINARY', col('own_code')), refer
+    });
 
-  login = async (payload: UserInput): Promise<any> => {
+    return user;
+  } catch(error: any) {
+    throw new Error(error.message);
+  }
+};
 
-    let condition = {};
-    let number = (payload.number === "string") ? "" : payload.number;
-    let email = (payload.email === "string") ? "" : payload.email;
+login = async (payload: UserInput): Promise<any> => {
 
-    if (number !== "" && email !== "") {
-      condition = { number: number, email: email };
-    } else if (email !== "") {
-      condition = { email: email };
-    } else if (number !== "") {
-      condition = { number: number };
-    }
-    interface login {
-      number: string;
-      email: string;
-      password: string;
-    }
-    let users: login | null = await userModel.findOne({
-      where: condition,
+  let condition = {};
+  let number = (payload.number === "string") ? "" : payload.number;
+  let email = (payload.email === "string") ? "" : payload.email;
+
+  if (number !== "" && email !== "") {
+    condition = { number: number, email: email };
+  } else if (email !== "") {
+    condition = { email: email };
+  } else if (number !== "") {
+    condition = { number: number };
+  }
+  interface login {
+    number: string;
+    email: string;
+    password: string;
+  }
+  let users: login | null = await userModel.findOne({
+    where: condition,
+    attributes: {
+      exclude: [
+        "deletedAt",
+        "cronStatus",
+        "createdAt",
+        "UID",
+        "antiphishing",
+        "registerType",
+        "statusType",
+        "tradingPassword",
+        "kycstatus",
+      ],
+    },
+    raw: true,
+  });
+
+  return users;
+};
+
+checkUserByPk = async (id: string): Promise<UserOuput | any> => {
+  const user = await userModel.findOne({
+    where: { id: id },
+    raw: true
+  });
+  return user;
+}
+
+  async getListOfUser(): Promise < UserOuput[] > {
+  try {
+
+    return await userModel.findAll({
+      include:
+      {
+        model: walletsModel,
+      }
+    });
+  } catch(error: any) {
+    throw new Error(error.message);
+  }
+}
+  async getListOfUserByLimit(offset: any, limit: any): Promise < UserOuput[] > {
+  try {
+    let offsets = parseInt(offset);
+    let limits = parseInt(limit);
+    return await userModel.findAll({
+      include:
+      {
+        model: walletsModel,
+      },
+      limit: limits,
+      offset: offsets,
+    });
+  } catch(error: any) {
+    throw new Error(error.message);
+  }
+}
+
+  async getListOfAdminProfit(): Promise < MarketProfitOuput[] > {
+  try {
+    return await MarketProfitModel.findAll({
+      attributes: [
+        [sequelize.fn('SUM', sequelize.col('profit')), 'profit'],
+        'coin_type',
+        [sequelize.fn('SUM', sequelize.col('fees')), 'fees'],
+        [sequelize.fn('SUM', sequelize.col('listing_fee')), 'listing_fee'],
+
+      ],
+      group: ['coin_type'],
+      raw: true
+    })
+
+  } catch(error: any) {
+    throw new Error(error.message);
+  }
+}
+  async getListOfUserActivity(): Promise < lastLoginOuput[] > {
+  try {
+    return await lastLoginModel.findAll({ raw: true });
+  } catch(error: any) {
+    throw new Error(error.message);
+  }
+}
+  async getListOfUserActivityByLimit(offset: string, limit: string): Promise < lastLoginOuput[] > {
+  try {
+    let offsets = parseInt(offset)
+      let limits = parseInt(limit)
+      return await lastLoginModel.findAll({
+      limit: limits,
+      offset: offsets
+    });
+  } catch(error: any) {
+    throw new Error(error.message);
+  }
+}
+  async getListOfUserActivityByIdLimit(userid: string, offset: string, limit: string): Promise < lastLoginOuput[] > {
+  try {
+    let offsets = parseInt(offset)
+      let limits = parseInt(limit)
+      return await lastLoginModel.findAll({
+      where: { user_id: userid },
+      limit: limits,
+      offset: offsets
+    });
+  } catch(error: any) {
+    throw new Error(error.message);
+  }
+}
+
+  async clearAllUserActivity(user_id: string): Promise < any > {
+  try {
+    let response = await lastLoginModel.destroy({ where: { user_id: user_id } });
+    return true;
+
+  } catch(error: any) {
+    throw new Error(error.message);
+  }
+}
+
+  async getUserDetailAllActivity(user_id: string): Promise < UserOuput | any > {
+  try {
+    return await userModel.findOne({
+      where: { id: user_id },
       attributes: {
         exclude: [
+          "password",
           "deletedAt",
           "cronStatus",
+          "updatedAt",
+          "createdAt",
           "createdAt",
           "UID",
           "antiphishing",
@@ -112,149 +239,24 @@ class userDal {
           "statusType",
           "tradingPassword",
           "kycstatus",
+          "TwoFA",
+          "otpToken", "own_code",
+          "refeer_code", "secret"
         ],
       },
-      raw: true,
-    });
-
-    return users;
-  };
-
-  checkUserByPk = async (id: string): Promise<UserOuput | any> => {
-    const user = await userModel.findOne({
-      where: { id: id },
-      raw: true
-    });
-    return user;
+      include: [
+        { model: assetModel, include: [{ model: tokensModel }, { model: globalTokensModel }] },
+        { model: withdrawModel, include: [{ model: tokensModel }, { model: globalTokensModel }] },
+        { model: depositModel },
+        { model: marketOrderModel, include: [{ model: tokensModel }, { model: globalTokensModel }] },
+        { model: orderModel, include: [{ model: tokensModel }, { model: globalTokensModel }] },
+        { model: lastLoginModel },
+      ]
+    })
+  } catch(error: any) {
+    throw new Error(error.message);
   }
-
-  async getListOfUser(): Promise<UserOuput[]> {
-    try {
-
-      return await userModel.findAll({
-        include:
-        {
-          model: walletsModel,
-        }
-      });
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  }
-  async getListOfUserByLimit(offset: any, limit: any): Promise<UserOuput[]> {
-    try {
-      let offsets = parseInt(offset);
-      let limits = parseInt(limit);
-      return await userModel.findAll({
-        include:
-        {
-          model: walletsModel,
-        },
-        limit: limits,
-        offset: offsets,
-      });
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  }
-
-  async getListOfAdminProfit(): Promise<MarketProfitOuput[]> {
-    try {
-      return await MarketProfitModel.findAll({
-        attributes: [
-          [sequelize.fn('SUM', sequelize.col('profit')), 'profit'],
-          'coin_type',
-          [sequelize.fn('SUM', sequelize.col('fees')), 'fees'],
-          [sequelize.fn('SUM', sequelize.col('listing_fee')), 'listing_fee'],
-
-        ],
-        group: ['coin_type'],
-        raw: true
-      })
-
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  }
-  async getListOfUserActivity(): Promise<lastLoginOuput[]> {
-    try {
-      return await lastLoginModel.findAll({ raw: true });
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  }
-  async getListOfUserActivityByLimit(offset: string, limit: string): Promise<lastLoginOuput[]> {
-    try {
-      let offsets = parseInt(offset)
-      let limits = parseInt(limit)
-      return await lastLoginModel.findAll({
-        limit: limits,
-        offset: offsets
-      });
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  }
-  async getListOfUserActivityByIdLimit(userid: string, offset: string, limit: string): Promise<lastLoginOuput[]> {
-    try {
-      let offsets = parseInt(offset)
-      let limits = parseInt(limit)
-      return await lastLoginModel.findAll({
-        where: { user_id: userid },
-        limit: limits,
-        offset: offsets
-      });
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  }
-
-  async clearAllUserActivity(user_id: string): Promise<any> {
-    try {
-      let response = await lastLoginModel.destroy({ where: { user_id: user_id } });
-      return true;
-
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  }
-
-  async getUserDetailAllActivity(user_id: string): Promise<UserOuput | any> {
-    try {
-      return await userModel.findOne({
-        where: { id: user_id },
-        attributes: {
-          exclude: [
-            "password",
-            "deletedAt",
-            "cronStatus",
-            "updatedAt",
-            "createdAt",
-            "createdAt",
-            "UID",
-            "antiphishing",
-            "registerType",
-            "statusType",
-            "tradingPassword",
-            "kycstatus",
-            "TwoFA",
-            "otpToken", "own_code",
-            "refeer_code", "secret"
-          ],
-        },
-        include: [
-          { model: assetModel, include: [{ model: tokensModel }, { model: globalTokensModel }] },
-          { model: withdrawModel, include: [{ model: tokensModel }, { model: globalTokensModel }] },
-          { model: depositModel },
-          { model: marketOrderModel, include: [{ model: tokensModel }, { model: globalTokensModel }] },
-          { model: orderModel, include: [{ model: tokensModel }, { model: globalTokensModel }] },
-          { model: lastLoginModel },
-        ]
-      })
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  }
+}
 }
 
 export default userDal;
