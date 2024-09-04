964cd2f3eb6a1c5a0bc1b38b4ee59ad94ba6c760
sunnymasih0253 <sunnyk.mdb@gmail.com>
2024-09-04 16:52:24 +0530

    merge code

--- src/server.ts
+++ src/server.ts
@@ -126,22 +126,22 @@ cron.schedule("*/10 * * * * *", async () => {
  * Cron for market order spot trading 
  */
 let isCronRunning = false;
-// cron.schedule('*/3 * * * *', async () => {  // Cron job runs every 5 seconds
-//   if (isCronRunning) {
-//     console.log("Previous cron job is still running. Skipping this execution.");
-//     return;
-//   }
-//   isCronRunning = true;
-//   try {
-//     const batchSize = 100;  // Define your batch size here
-//     await service.cronMarket.processOrdersInBatches(batchSize);
-//   } catch (error) {
-//     console.error("Error in cron job:", error);
-//   }
-//   finally {
-//     isCronRunning = false;
-//   }
-// });
+cron.schedule('*/3 * * * *', async () => {  // Cron job runs every 5 seconds
+  if (isCronRunning) {
+    console.log("Previous cron job is still running. Skipping this execution.");
+    return;
+  }
+  isCronRunning = true;
+  try {
+    const batchSize = 100;  // Define your batch size here
+    await service.cronMarket.processOrdersInBatches(batchSize);
+  } catch (error) {
+    console.error("Error in cron job:", error);
+  }
+  finally {
+    isCronRunning = false;
+  }
+});
 
 
 
